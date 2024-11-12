8d40ac6e2abbb8366a3ebdcad05ce227a8784a91
Jiten <jpalaparthi@gmail.com>
2024-11-12 08:34:45 +0530

    add new function

--- shape/shape.go
+++ shape/shape.go
@@ -5,3 +5,7 @@ import "fmt"
 func What() {
 	fmt.Println("This shape package contains Rect,Square and Cuboid shapes")
 }
+
+func Why() {
+	fmt.Println("This package is required to perform shape based operations")
+}
